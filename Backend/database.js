f6251aaf90b20a9703cf3fcb6d025fa1d45633ce
KMK <103745547+KMKgit@users.noreply.github.com>
2023-12-17 23:39:53 +0200

    YesYes

--- Backend/database.js
+++ Backend/database.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "KMKsysteem123", //add your password
+    password: "", //add your password
     database: "testWad",
     host: "localhost",
     port: "5432"
